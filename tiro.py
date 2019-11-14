c2ea9861fe749ac8ac92240219da39c239cfecc2
JFRodriguezH <jf.rodriguezh@uniandes.edu.co>
2019-11-14 17:47:33 +0000

    Titulos y ejes en la figura

--- tiro.py
+++ tiro.py
@@ -8,12 +8,15 @@ x = data[:,1]
 y = data[:,2]
 vx = data[:,3]
 vy = data[:,4]
-plt.plot(x, y)
+plt.plot(x, y, label="Sin fricción")
 data = np.loadtxt("friccion.dat")
 t = data[:,0]
 x = data[:,1]
 y = data[:,2]
 vx = data[:,3]
 vy = data[:,4]
-plt.plot(x, y)
+plt.plot(x, y, label="Con fricción")
+plt.legend(loc="upper right")
+plt.ylabel("Y")
+plt.xlabel("X")
 plt.savefig("friccion.png")
