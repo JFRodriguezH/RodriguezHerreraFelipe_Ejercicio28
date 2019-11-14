c64a2b8c231bcf4d1fe715308fd0bb2f71094889
JFRodriguezH <jf.rodriguezh@uniandes.edu.co>
2019-11-14 17:35:38 +0000

    El parametro k deja de ser constante. Entra como parametro en la funcion

--- tiro.cpp
+++ tiro.cpp
@@ -2,11 +2,10 @@
 #include <fstream>
 #include <math.h>
 
-void rk(double h, double & x, double & y, double & vy, double & vx, double & t);
+void rk(double k, double h, double & x, double & y, double & vy, double & vx, double & t);
 std::ofstream outfile;
 
-double const k = 0.07;
-double const n = 2.0;
+double const n = 1.0;
 double const g = 9.81;
 
 double const t_0 = 0.0;
@@ -16,8 +15,8 @@ double const x_0 = 0.0;
 double const vx_0 = 10.0;
 
 int main(){
-	
 	double h = 0.01;
+    double k = 0.7;
 	double t = t_0;
 	double y = y_0;
 	double vy = vy_0;
@@ -26,10 +25,10 @@ int main(){
 	
 	outfile.open("tiro.dat");
     outfile << t << " "<< x << " " << y << " "<< vx << " " << vy << std::endl;
-	rk(h, x, y, vx, vy, t);
+	rk(k, h, x, y, vx, vy, t);
     while(y>0){
         outfile << t << " "<< x << " " << y << " "<< vx << " " << vy << std::endl;
-		rk(h, x, y, vx, vy, t);
+		rk(k, h, x, y, vx, vy, t);
     }
     outfile.close();
 	
@@ -40,7 +39,7 @@ int main(){
 
 /*d^2 y/dt^2 = -g -k vy^n vy/|V|*/
 
-void rk(double h, double & x, double & y, double & vx, double & vy, double & t){
+void rk(double k, double h, double & x, double & y, double & vx, double & vy, double & t){
     double k1x , k2x, k3x, k4x, k1y, k2y, k3y, k4y, k1vx, k2vx, k3vx, k4vx, k1vy, k2vy, k3vy, k4vy;
     double V = sqrt(vx*vx + vy*vy);
     
@@ -70,12 +69,3 @@ void rk(double h, double & x, double & y, double & vx, double & vy, double & t){
     vx += (h/6.0)*(k1vx + 2.0*k2vx + 2.0*k3vx + k4vx);
     vy += (h/6.0)*(k1vy + 2.0*k2vy + 2.0*k3vy + k4vy);
 }
-
-/*outfile.open("explicit_01.dat");
-    for(int i=0; i<n+1; i++){
-        outfile << t << " " << y <<endl;
-        t += delta;
-        y = expl(y, wDelta);
-    }
-	outfile << delta << " " << 0 << endl;
-    outfile.close();*/
